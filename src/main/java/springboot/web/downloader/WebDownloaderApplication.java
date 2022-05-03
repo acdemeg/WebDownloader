a72c72eb7097aa90e5c46e01c114765860a2ac30
acdemeg <v.krylov1996@gmail.com>
2022-05-03 19:59:05 +0600

    Initial commit

---
+++ src/main/java/springboot/web/downloader/WebDownloaderApplication.java
@@ -0,0 +1,13 @@
+package springboot.web.downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/web/downloader/WebDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.web.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

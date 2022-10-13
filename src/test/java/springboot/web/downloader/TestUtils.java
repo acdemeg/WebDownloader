68cef7a8accbbbf9ced9225b725e671aa9397419
acdemeg <v.krylov1996@gmail.com>
2022-10-13 23:04:00 +0600

    [*] Small fix test

--- src/test/java/springboot/web/downloader/TestUtils.java
+++ src/test/java/springboot/web/downloader/TestUtils.java
@@ -9,8 +9,8 @@ import java.lang.reflect.Modifier;
 
 public class TestUtils {
 
-    private final static String sitesPath = System.getProperty("user.home") + "/test/sites/";
-    private final static String archivedPath = System.getProperty("user.home") + "/test/archived/";
+    private final static String SITES_PATH = System.getProperty("user.home") + "/test/sites/";
+    private final static String ARCHIVED_PATH = System.getProperty("user.home") + "/test/archived/";
 
     public static void setFinalStaticField(String fieldName, String value) throws NoSuchFieldException, IllegalAccessException {
         Field field = WebDownloader.class.getField(fieldName);
@@ -22,14 +22,14 @@ public class TestUtils {
     }
 
     public static void prepareTestEnv() throws IOException, NoSuchFieldException, IllegalAccessException {
-        FileUtils.forceMkdir(new File(sitesPath));
-        FileUtils.forceMkdir(new File(archivedPath));
-        setFinalStaticField("baseSites", sitesPath);
-        setFinalStaticField("baseArchived", archivedPath);
+        FileUtils.forceMkdir(new File(SITES_PATH));
+        FileUtils.forceMkdir(new File(ARCHIVED_PATH));
+        setFinalStaticField("BASE_SITES", SITES_PATH);
+        setFinalStaticField("BASE_ARCHIVED", ARCHIVED_PATH);
     }
 
     public static void discardTestEnv() throws IOException {
-        FileUtils.cleanDirectory(new File(sitesPath));
-        FileUtils.cleanDirectory(new File(archivedPath));
+        FileUtils.cleanDirectory(new File(SITES_PATH));
+        FileUtils.cleanDirectory(new File(ARCHIVED_PATH));
     }
 }
